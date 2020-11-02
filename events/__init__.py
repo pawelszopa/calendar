52ff912f45d9348742ce759239fd851cb3fa0df0
Pawel Szopa <expiren@gmail.com>
2020-11-02 22:00:32 +0100

    add class event method add

---
+++ events/__init__.py
@@ -0,0 +1,1 @@
+from events.events import Events
---
+++ events/events.py
@@ -0,0 +1,7 @@
+class Events:
+    def __init__(self):
+        self._events = []
+    
+    def add_event(self,event):
+        self._events.append(event)
+    
